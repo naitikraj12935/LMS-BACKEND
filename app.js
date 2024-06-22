7bb94054a6d834de0ae7f72339de3bb75989b664
Naitik Raj <115349374+naitikraj12935@users.noreply.github.com>
2024-06-22 19:59:11 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 app.use(cors({
-    origin: "https://shiny-valkyrie-d6eb9e.netlify.app",
+    origin: "https://coursify1.netlify.app",
     credentials: true
 }));
 
